f73fb6cf37799b27ad76152896d8cc796ca8e607
Ekirapapaul <ekirapapaul@gmail.com>
2021-06-04 00:20:30 +0300

    Fix linting errors on admissiondetails

--- ui/app/common/displaycontrols/admissiondetails/directives/admissionDetails.js
+++ ui/app/common/displaycontrols/admissiondetails/directives/admissionDetails.js
@@ -3,24 +3,24 @@
 angular.module('bahmni.common.displaycontrol.admissiondetails')
     .directive('admissionDetails', ['bedService', 'observationsService', 'spinner',
         function (bedService, observationsService, spinner) {
-        var controller = function ($scope) {
-            $scope.showDetailsButton = function (encounter) {
-                return $scope.params && $scope.params.showDetailsButton && !encounter.notes;
+            var controller = function ($scope) {
+                $scope.showDetailsButton = function (encounter) {
+                    return $scope.params && $scope.params.showDetailsButton && !encounter.notes;
+                };
+                $scope.toggle = function (element) {
+                    element.show = !element.show;
+                };
+                init($scope);
             };
-            $scope.toggle = function (element) {
-                element.show = !element.show;
+            var isReady = function ($scope) {
+                return !_.isUndefined($scope.patientUuid) && !_.isUndefined($scope.visitSummary);
             };
-            init($scope);
-        };
-        var isReady = function ($scope) {
-            return !_.isUndefined($scope.patientUuid) && !_.isUndefined($scope.visitSummary);
-        };
-        var onReady = function ($scope) {
-            var visitUuid = _.get($scope.visitSummary, 'uuid');
-            bedService.getAssignedBedForPatient($scope.patientUuid, visitUuid).then(function (bedDetails) {
-                $scope.bedDetails = bedDetails;
-            });
-        };
+            var onReady = function ($scope) {
+                var visitUuid = _.get($scope.visitSummary, 'uuid');
+                bedService.getAssignedBedForPatient($scope.patientUuid, visitUuid).then(function (bedDetails) {
+                    $scope.bedDetails = bedDetails;
+                });
+            };
 
             var getAdmissionType = function ($scope) {
                 const conceptNames = ["ADT TYPE"];
@@ -31,30 +31,30 @@ angular.module('bahmni.common.displaycontrol.admissiondetails')
                 }));
             };
 
-        var init = function ($scope) {
-            var stopWatching = $scope.$watchGroup(['patientUuid', 'visitSummary'], function () {
-                if (isReady($scope)) {
-                    stopWatching();
-                    onReady($scope);
-                }
-            });
+            var init = function ($scope) {
+                var stopWatching = $scope.$watchGroup(['patientUuid', 'visitSummary'], function () {
+                    if (isReady($scope)) {
+                        stopWatching();
+                        onReady($scope);
+                    }
+                });
 
-            $scope.isDataPresent = function () {
-                if (!$scope.visitSummary || (!$scope.visitSummary.admissionDetails && !$scope.visitSummary.dischargeDetails)) {
-                    return $scope.$emit("no-data-present-event") && false;
-                }
+                $scope.isDataPresent = function () {
+                    if (!$scope.visitSummary || (!$scope.visitSummary.admissionDetails && !$scope.visitSummary.dischargeDetails)) {
+                        return $scope.$emit("no-data-present-event") && false;
+                    }
 
-                return true;
+                    return true;
+                };
             };
-        };
-        return {
-            restrict: 'E',
-            controller: controller,
-            templateUrl: "../common/displaycontrols/admissiondetails/views/admissionDetails.html",
-            scope: {
-                params: "=",
-                patientUuid: "=",
-                visitSummary: "="
-            }
-        };
-    }]);
+            return {
+                restrict: 'E',
+                controller: controller,
+                templateUrl: "../common/displaycontrols/admissiondetails/views/admissionDetails.html",
+                scope: {
+                    params: "=",
+                    patientUuid: "=",
+                    visitSummary: "="
+                }
+            };
+        }]);
